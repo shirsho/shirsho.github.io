7e7f643d8ec05e226d66cf97f0eeba9ccb0d7417
shirsho <srshirsho@gmail.com>
2014-07-25 14:53:22 -0400

    Fixed page reload onclick issue

--- javascripts/myJavascript.js
+++ javascripts/myJavascript.js
@@ -3,14 +3,13 @@ function goToAbout()
 	$('html, body').animate({
         scrollTop: $("#about").offset().top
     }, 1000);
-	return false;
+	
 }
 function goToEngineeringExperience()
 {
 	$('html, body').animate({
         scrollTop: $("#engineering-experience").offset().top
     }, 1000);
-    return false;
 
 }
 function goToMusic()
@@ -18,5 +17,5 @@ function goToMusic()
 	$('html, body').animate({
         scrollTop: $("#music").offset().top
     }, 1000);
-    return false;
+   
 }
