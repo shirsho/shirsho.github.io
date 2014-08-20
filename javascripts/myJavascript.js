17eb4749b7efed56c2dfe1125850a0aedf857a42
shirsho <srshirsho@gmail.com>
2014-08-20 02:02:18 -0400

    Finalized sections of the site, pending edited text content and slight animation

--- javascripts/myJavascript.js
+++ javascripts/myJavascript.js
@@ -1,21 +1,50 @@
+var scrollTime = 800;
 function goToAbout()
 {
 	$('html, body').animate({
         scrollTop: $("#about").offset().top
-    }, 1000);
+    }, scrollTime);
 	
 }
 function goToEngineeringExperience()
 {
 	$('html, body').animate({
         scrollTop: $("#engineering-experience").offset().top
-    }, 1000);
+    }, scrollTime);
 
 }
 function goToMusic()
 {
 	$('html, body').animate({
         scrollTop: $("#music").offset().top
-    }, 1000);
+    }, scrollTime);
    
+}
+
+function goToProjects()
+{
+    $('html, body').animate({
+        scrollTop: $("#projects").offset().top
+    }, scrollTime);
+   
+}
+
+/*
+window.addEventListener("onload", animation);
+var animation = function onloadAnimation()
+{
+    $("#title").fadeIn(5400, function() {
+        // Animation complete.
+        $("#title").animate({'marginTop' : "-=20px"});    
+    });    
+}
+*/
+window.onload = function() {    
+    $("#title-and-contents").animate({'marginTop' : "-=10px"}).show("fade", {},  {duration:1200}).dequeue();
+};
+
+function highlightDiv(div){
+    $("div").hover(function(){
+        $(this).css("background": "blue");
+    });
 }
